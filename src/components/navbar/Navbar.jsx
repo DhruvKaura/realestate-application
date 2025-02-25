6f5eb8e6272e46e380a21e92a68d08b37b5726af
DhruvKaura <dhruvkaura25@gmail.com>
2025-02-25 11:44:57 +0530

    added the responsive navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 import "./Navbar.scss"
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav>
       <div className="left">
         <a href="" className="logo">
-          <img  src="/public/logo.png" alt="" />
+          <img  src="/logo.png" alt="" />
           <span>Find Homes</span>
         </a>
         <a href="">Home</a>
@@ -16,6 +18,17 @@ const Navbar = () => {
       <div className="right">
         <a href="">Sign in</a>
         <a href="" className="register">Sign up</a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" onClick={()=>setOpen((prev)=>!prev)} />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Agents</a>
+        <a href="">Sign in</a>
+        <a href="">Sign up</a>
+        </div>
       </div>
     </nav>
   )
