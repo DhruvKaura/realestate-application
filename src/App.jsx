4ac3fce6f737629c74bbbeb8a388dc0611ad24c9
DhruvKaura <dhruvkaura25@gmail.com>
2025-02-26 18:14:10 +0530

    Filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import "./layout.scss"
-import "./components/navbar/Navbar"
-import Navbar from "./components/navbar/Navbar"
 import HomePage from "./routes/homePage/homePage"
 import ListPage from "./routes/listPage/listPage"
 import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router";
 import { Link } from "react-router-dom";
+import Layout from "./routes/layout/layout"
+import Login from "./routes/login/login";
+import SinglePage from "./routes/singlePage/singlePage";
 
 function App() {
   const router = createBrowserRouter([
     {path:"/",
-    element: <HomePage />,
-    },
-    {
-      path: "/list",
-      element: <ListPage/>
-    },
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/list",
+        element: <ListPage/>
+      },
+      {
+        path: "/login",
+        element: <Login/>
+      },
+      {
+        path: "/:id",
+        element: <SinglePage/>
+      },
+    ]
+    }
   ]);
   return (
-    // <div className="layout">
-    //   <div className="navbar">
-    //     <Navbar />
-    //   </div>
-    //   <div className="content">
-    //     <HomePage/>
-    //   </div>
-    // </div>
+
     <RouterProvider router={router}/>
   )
 }
--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { listData } from '../../lib/dummydata'
+import "./listPage.scss"
+import Filter from '../../components/filter/filter'
 
 const ListPage = () => {
+  const data = listData
   return (
     <div className='listPage'>
-        listpage
+        <div className="listContainer">
+          <div className="wrapper">
+            <Filter/>
+          </div>
+        </div>
+        <div className="mapContainer">Map</div>
     </div>
   )
 }
