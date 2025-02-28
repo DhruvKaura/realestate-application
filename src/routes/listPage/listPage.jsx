7742aa0b2c724247960f8eccc8bb5fdc9ef2913b
DhruvKaura <dhruvkaura25@gmail.com>
2025-02-28 13:50:18 +0530

    map added

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -3,6 +3,7 @@ import { listData } from '../../lib/dummydata'
 import "./listPage.scss"
 import Filter from '../../components/filter/filter'
 import Card from '../../components/card/card'
+import Map from '../../components/map/map'
 
 const ListPage = () => {
   const data = listData
@@ -16,7 +17,9 @@ const ListPage = () => {
             ))}
           </div>
         </div>
-        <div className="mapContainer">Map</div>
+        <div className="mapContainer">
+          <Map items={data}/>
+        </div>
     </div>
   )
 }
